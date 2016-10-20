fe1fe5b2d8b105695a24f3f7781a5372f0f755ce
Brandon Morrell <brandonmorrell@Brandons-MacBook-Pro.local>
2016-10-20 19:17:23 -0400

    Brandons Code

--- src/ilite/us/robot/Robot.java
+++ src/ilite/us/robot/Robot.java
@@ -2,17 +2,22 @@
 package ilite.us.robot;
 
 
+import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.SampleRobot;
 
+
 public class Robot extends SampleRobot {
+private CANTalon drive;
 
 	public Robot() {
     }
     
     public void robotInit() {
+    	drive = new CANTalon(0);
     }
 
     public void autonomous() {
+    	drive.set(1);
     }
 
     public void operatorControl() {
