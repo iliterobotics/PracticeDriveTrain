c699028cc998380bbca032d069939e57498408ea
Samuel Spelsberg <Samuel Spelsberg>
2016-11-09 17:55:00 -0500

    Commit for the practice drive train

--- src/ilite/us/robot/Robot.java
+++ src/ilite/us/robot/Robot.java
@@ -3,26 +3,53 @@ package ilite.us.robot;
 
 
 import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Joystick.AxisType;
 import edu.wpi.first.wpilibj.SampleRobot;
 
 
 public class Robot extends SampleRobot {
-private CANTalon drive;
+private CANTalon backRight, frontRight;
+private CANTalon backLeft, frontLeft;
+private Joystick leftStick, rightStick;
 
-	public Robot() {
+	public Robot() 
+	{
+		robotInit();
     }
     
-    public void robotInit() {
-    	drive = new CANTalon(0);
+    public void robotInit() 
+    {
+    	frontRight = new CANTalon(4);
+    	frontLeft = new CANTalon(1);
+    	backRight = new CANTalon(2);
+    	backLeft = new CANTalon(3);
+    	leftStick = new Joystick(0);
+    	rightStick = new Joystick(1);
     }
 
-    public void autonomous() {
-    	drive.set(1);
+    public void autonomous() 
+    {
     }
 
-    public void operatorControl() {
+    public void operatorControl() 
+    {
+    	updateLeft(leftStick.getAxis(AxisType.kY));
+    	updateRight(rightStick.getAxis(AxisType.kY));
     }
 
-    public void test() {
+    public void test() 
+    {
+    	
+    }
+    
+    public void updateLeft(double speed){
+    	frontLeft.set(speed);
+    	backLeft.set(speed);
+    }
+    
+    public void updateRight(double speed){
+    	frontRight.set(speed);
+    	backRight.set(speed);
     }
 }
