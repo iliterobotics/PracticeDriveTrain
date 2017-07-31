b8ef689731a74a10ebd09437a0e4b699037bfb26
Unknown <brandon@morrellworld.net>
2017-07-31 14:57:43 -0400

    Just a simple test

---
+++ src/ilite/us/robot/DriveStraight.java
@@ -0,0 +1,17 @@
+package ilite.us.robot;
+
+
+public class DriveStraight {
+
+	private long startTime = System.currentTimeMillis();
+	private double speed;
+	private double seconds;
+	
+	private DriveStraight(double speed, double seconds)
+	{
+		this.speed = speed;
+		this.seconds = seconds;
+	}
+	
+	private
+}
